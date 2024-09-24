79d7617d81cbecac9c233fcb2dfd4a4d4fcee524
adityathakkar17 <adityathakkar33@gmail.com>
2024-09-24 02:04:57 -0500

    updated page.ts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import About from "@/components/about";
-import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
